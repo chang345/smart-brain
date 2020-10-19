926541df615ca0aa6ad0a9db5d43ae7bbfab97e0
Sang Chang <sangchang@hotmail.com>
2020-10-19 17:11:05 -0500

    fixed config again 2

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
-if (process.env.NODE_ENV === 'prod') {
-  module.exports = require('./prod');
-} else {
-  module.exports = require('./dev');
-}
-
+// if (process.env.NODE_ENV === 'prod') {
+//   module.exports = require('./prod');
+// } else {
+//   module.exports = require('./dev');
+// }
+module.exports = require('./prod');
