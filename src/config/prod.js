1931889cd97d6f823377ee3962f922ab3887905b
Sang Chang <sangchang@hotmail.com>
2020-10-19 17:17:07 -0500

    add logging

--- src/config/prod.js
+++ src/config/prod.js
@@ -1,4 +1,6 @@
-const apiHost = `https://${process.env.API_HOST}`;
+console.log('NODE_ENV: ' + process.env.NODE_ENV);
+console.log('API_HOST: ' + process.env.API_HOST);
+const apiHost = 'https://' + process.env.API_HOST;
 module.exports = {
   apiHost: apiHost,
 };
